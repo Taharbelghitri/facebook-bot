c9f0a88c90dc63f85bb42ddd5f2d48977d44337b
tahar belhghitri <47835966+Taharbelghitri@users.noreply.github.com>
2021-11-04 21:07:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extends: true }));
+
+app.get("/webhook", (req, res) => {
+  if (req.query["hub.mode"] && req.query["hub.verify_token"] === "tuxedo_cat") {
+    res.status(200).send(req.query["hub.challenge"]);
+  } else {
+    res.status(403).end();
+  }
+});
+
+/* Handling all messenges */
+app.post("/webhook", (req, res) => {
+  console.log(req.body);
+  if (req.body.object === "page") {
+    req.body.entry.forEach((entry) => {
+      entry.messaging.forEach((event) => {
+        if (event.message && event.message.text) {
+          sendMessage(event);
+        }
+      });
+    });
+    res.status(200).end();
+  }
+});
+
+app.listen(5000, () => console.log("servevr started at 5000"));
