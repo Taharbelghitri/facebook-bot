0fe407503e090886ac6558aeeb1c03ab1f08df00
tahar belhghitri <47835966+Taharbelghitri@users.noreply.github.com>
2021-11-04 22:23:56 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -26,6 +26,35 @@ app.post("/webhook", (req, res) => {
   }
 });
 
+const request = require("request");
+
+function sendMessage(event) {
+  let sender = event.sender.id;
+  let text = event.message.text;
+
+  request(
+    {
+      url: "https://graph.facebook.com/v2.6/me/messages",
+      qs: {
+        access_token:
+          "EAAI0k6ZAYaRIBAMZBDALxheMzhrhUxy7L1t9MBlBM3GtdzgiZCO4zEZAtidDZBrDs7x0Q3R5eRZCNCPDXGD504Ycu2ZAEh52J7M5U0o1r3tUcQwhAFGfny9brheXPCPw4AKAnX0pRpwqGJlftjuRpefizmcZBgrZCMI9bmrbEM1mNZBkHjXbAjgiKbvZB2y1sNwk6ScnJQoBqHVzAZDZD",
+      },
+      method: "POST",
+      json: {
+        recipient: { id: sender },
+        message: { text: text },
+      },
+    },
+    function (error, response) {
+      if (error) {
+        console.log("Error sending message: ", error);
+      } else if (response.body.error) {
+        console.log("Error: ", response.body.error);
+      }
+    }
+  );
+}
+
 app.listen(process.env.PORT || 5000, () =>
   console.log("servevr started at 5000")
 );
