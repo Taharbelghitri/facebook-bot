88c0a633e4b4b0db0c7710405e315ccc6204742c
tahar belhghitri <47835966+Taharbelghitri@users.noreply.github.com>
2021-11-05 12:35:56 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ function sendMessage(event) {
       url: "https://graph.facebook.com/v7.0/me/messages/",
       qs: {
         access_token:
-          "EAANZBQwqZAZA3ABAMltfFvokLzwaMeKPZBOVYCwTS5fZCIVzyWWy5MD3cVwf6QBKeIiMEVjTiM0deux95Ra7NIgzkZBPpeeY5N9jKYep1XXwPehzARSu0d315UvEPErULcq4j6VqT1EUCfNhqZAzqvU2shxVfjQRew5kLAGZByJHK94U4PX9ZCRUZA",
+          "EAAD3SH5DhBABAH2AKtYUkyfHXB4KBzqEXeN5mZAabbsob1fEPsaBmUwT2A7OcMHUa0dDOQZAKXVQUEa0LvdZAnvBAEvBpCugwgh24OkCaQa12EZCWmQRRMEwfMZADeM7m2kMenj9ZCQhcfaUecUWGlSK7yTpbyF1P8gTdjE2i8YVPXyNGV2oUW",
+        //   "EAANZBQwqZAZA3ABAMltfFvokLzwaMeKPZBOVYCwTS5fZCIVzyWWy5MD3cVwf6QBKeIiMEVjTiM0deux95Ra7NIgzkZBPpeeY5N9jKYep1XXwPehzARSu0d315UvEPErULcq4j6VqT1EUCfNhqZAzqvU2shxVfjQRew5kLAGZByJHK94U4PX9ZCRUZA",
       },
       method: "POST",
       json: {
