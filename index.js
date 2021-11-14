081fd6f164a07c95ff0f03db68d0e239a4db7ae7
tahar belhghitri <47835966+Taharbelghitri@users.noreply.github.com>
2021-11-14 01:13:23 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -34,34 +34,71 @@ function sendMessage(event) {
   let sender = event.sender.id;
   let text = event.message.text;
   console.log("sender : " + sender);
+  const i = 10;
+  const sender = async (i = 0) => {
+    if (i === 10) {
+      Promise(reseolve).then(() =>
+        request(
+          {
+            url: "https://graph.facebook.com/v7.0/me/messages/",
+            qs: {
+              access_token:
+                //"EAAD3SH5DhBABAFZCtVGvVBN71k6fzZC9ZAWyoS2XO8tQwU7627MdMRQoyJ1jJvQNhi0ioDBv16NbZCykWh2ZCgeWHeTLrvK0eODOmSmRUTS4gU7269DBzwziZC02i5VDm97Izh6E4ttZAAO1VDAOr98j0CYuAZCbVnYYGkwqOcnhjf57SrtJf8wM",
 
-  request(
-    {
-      url: "https://graph.facebook.com/v7.0/me/messages/",
-      qs: {
-        access_token:
-          //"EAAD3SH5DhBABAFZCtVGvVBN71k6fzZC9ZAWyoS2XO8tQwU7627MdMRQoyJ1jJvQNhi0ioDBv16NbZCykWh2ZCgeWHeTLrvK0eODOmSmRUTS4gU7269DBzwziZC02i5VDm97Izh6E4ttZAAO1VDAOr98j0CYuAZCbVnYYGkwqOcnhjf57SrtJf8wM",
+                //    "EAAsA6fD3O0oBAJYm2q6XNffl9M482KYst0l6Ye5sIKK7MuEKduK9YhhB7O61BPlvZCYlyj6xIMZCZAzpwTD5CZCvml9t83iwoxb9UXsUkkX1JdZAS7qqcTmJrqCKR6sFnZANSSf390pNB2XZBA4w9OmioNlJePoXJNIiXDU3rw9ZC5dy1TJNoHTvq1PDsadBgjfKr8FHq4CndAZDZD",
 
-          //    "EAAsA6fD3O0oBAJYm2q6XNffl9M482KYst0l6Ye5sIKK7MuEKduK9YhhB7O61BPlvZCYlyj6xIMZCZAzpwTD5CZCvml9t83iwoxb9UXsUkkX1JdZAS7qqcTmJrqCKR6sFnZANSSf390pNB2XZBA4w9OmioNlJePoXJNIiXDU3rw9ZC5dy1TJNoHTvq1PDsadBgjfKr8FHq4CndAZDZD",
+                // "EAAsA6fD3O0oBAGaOlPEwAZBxAZCX6lgPwdEvLIFU7i2GlXag1BgRlHPoZBMt9RGDdBSKFacQYymPzSUP4oP5o953yD9rmDW7YK4deM8F8VaayFoHyK7OQ9s0bZAoZBZCOFuOxFYFZBCmVZAHAb2mqf2ulMShlIGEM6JiguMce1FVsUcAwZAX7lm77w7LT1MKVWDutBsbqIZByZBygZDZD",
 
-          // "EAAsA6fD3O0oBAGaOlPEwAZBxAZCX6lgPwdEvLIFU7i2GlXag1BgRlHPoZBMt9RGDdBSKFacQYymPzSUP4oP5o953yD9rmDW7YK4deM8F8VaayFoHyK7OQ9s0bZAoZBZCOFuOxFYFZBCmVZAHAb2mqf2ulMShlIGEM6JiguMce1FVsUcAwZAX7lm77w7LT1MKVWDutBsbqIZByZBygZDZD",
-
-          process.env.PAGE_TOKEN,
-      },
-      method: "POST",
-      json: {
-        recipient: { id: sender },
-        message: { text: text },
-      },
-    },
-    function (error, response) {
-      if (error) {
-        console.log("Error sending message: ", error);
-      } else if (response.body.error) {
-        console.log("Error: ", response.body.error);
-      }
+                process.env.PAGE_TOKEN,
+            },
+            method: "POST",
+            json: {
+              recipient: { id: sender },
+              message: { text: text },
+            },
+          },
+          function (error, response) {
+            if (error) {
+              console.log("Error sending message: ", error);
+            } else if (response.body.error) {
+              console.log("Error: ", response.body.error);
+            }
+            else {
+              sender(i++)
+            }
+          }
+        )
+      );
     }
-  );
+  };
+  sender();
+  // request(
+  //   {
+  //     url: "https://graph.facebook.com/v7.0/me/messages/",
+  //     qs: {
+  //       access_token:
+  //         //"EAAD3SH5DhBABAFZCtVGvVBN71k6fzZC9ZAWyoS2XO8tQwU7627MdMRQoyJ1jJvQNhi0ioDBv16NbZCykWh2ZCgeWHeTLrvK0eODOmSmRUTS4gU7269DBzwziZC02i5VDm97Izh6E4ttZAAO1VDAOr98j0CYuAZCbVnYYGkwqOcnhjf57SrtJf8wM",
+
+  //         //    "EAAsA6fD3O0oBAJYm2q6XNffl9M482KYst0l6Ye5sIKK7MuEKduK9YhhB7O61BPlvZCYlyj6xIMZCZAzpwTD5CZCvml9t83iwoxb9UXsUkkX1JdZAS7qqcTmJrqCKR6sFnZANSSf390pNB2XZBA4w9OmioNlJePoXJNIiXDU3rw9ZC5dy1TJNoHTvq1PDsadBgjfKr8FHq4CndAZDZD",
+
+  //         // "EAAsA6fD3O0oBAGaOlPEwAZBxAZCX6lgPwdEvLIFU7i2GlXag1BgRlHPoZBMt9RGDdBSKFacQYymPzSUP4oP5o953yD9rmDW7YK4deM8F8VaayFoHyK7OQ9s0bZAoZBZCOFuOxFYFZBCmVZAHAb2mqf2ulMShlIGEM6JiguMce1FVsUcAwZAX7lm77w7LT1MKVWDutBsbqIZByZBygZDZD",
+
+  //         process.env.PAGE_TOKEN,
+  //     },
+  //     method: "POST",
+  //     json: {
+  //       recipient: { id: sender },
+  //       message: { text: text },
+  //     },
+  //   },
+  //   function (error, response) {
+  //     if (error) {
+  //       console.log("Error sending message: ", error);
+  //     } else if (response.body.error) {
+  //       console.log("Error: ", response.body.error);
+  //     }
+  //   }
+  // );
 }
 
 app.listen(process.env.PORT || 5000, () =>
