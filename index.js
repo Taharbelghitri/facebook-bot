4c9a59053bd57133c7387badd1372117dd78cd02
tahar belhghitri <47835966+Taharbelghitri@users.noreply.github.com>
2021-11-14 18:33:20 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const request = require("request");
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -33,45 +34,45 @@ function sendMessage(event) {
   let sender = event.sender.id;
   let text = event.message.text;
 
-  axios({
-    method: "POST",
-    url: "https://graph.facebook.com/v7.0/me/messages/",
-    headers: {
-      access_token: process.env.PAGE_TOKEN,
-    },
-    data: {
-      recipient: { id: sender },
-      message: { text: text },
-    },
-  })
-    .then((data) => {
-      if (data.body.error) console.log("Error: ", response.body.error);
-      else {
-        console.log("message sented ");
-      }
-    })
-    .catch((err) => console.log(err));
-
-  // request(
-  //   {
-  //     url: "https://graph.facebook.com/v7.0/me/messages/",
-  //     qs: {
-  //       access_token: process.env.PAGE_TOKEN,
-  //     },
-  //     method: "POST",
-  //     json: {
-  //       recipient: { id: sender },
-  //       message: { text: text },
-  //     },
+  // axios({
+  //   method: "POST",
+  //   url: "https://graph.facebook.com/v7.0/me/messages/",
+  //   headers: {
+  //     access_token: process.env.PAGE_TOKEN,
   //   },
-  //   function (error, response) {
-  //     if (error) {
-  //       console.log("Error sending message: ", error);
-  //     } else if (response.body.error) {
-  //       console.log("Error: ", response.body.error);
+  //   data: {
+  //     recipient: { id: sender },
+  //     message: { text: text },
+  //   },
+  // })
+  //   .then((data) => {
+  //     if (data.body.error) console.log("Error: ", response.body.error);
+  //     else {
+  //       console.log("message sented ");
   //     }
-  //   }
-  // );
+  //   })
+  //   .catch((err) => console.log(err));
+
+  request(
+    {
+      url: "https://graph.facebook.com/v7.0/me/messages/",
+      qs: {
+        access_token: process.env.PAGE_TOKEN,
+      },
+      method: "POST",
+      json: {
+        recipient: { id: sender },
+        message: { text: text },
+      },
+    },
+    function (error, response) {
+      if (error) {
+        console.log("Error sending message: ", error);
+      } else if (response.body.error) {
+        console.log("Error: ", response.body.error);
+      }
+    }
+  );
 }
 
 app.listen(process.env.PORT || 5000, () =>
